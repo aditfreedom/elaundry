fa5713923e7cf3bc55a3bfa26cc374aa314509f4
Adit <37005476+aditfreedom@users.noreply.github.com>
2023-12-27 16:12:43 +0700

    Push pertama

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('homepage');
+});
+
+
+//Route Admin Area
+Route::get('/user', [AdminController::class, 'index']);
+
