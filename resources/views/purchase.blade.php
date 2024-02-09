0396fcde6ce42ae91f57095bdaaeec3ffea06629
Adit <37005476+aditfreedom@users.noreply.github.com>
2024-02-09 11:31:11 +0700

    Update

--- resources/views/purchase.blade.php
+++ resources/views/purchase.blade.php
@@ -70,9 +70,9 @@
 
                         <div class="d-flex m-3 me-0">
                             <button hidden class="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-primary"></i></button>
-                            <a href="#" class="position-relative me-4 my-auto">
+                            <a href="{{url('orderan')}}" class="position-relative me-4 my-auto">
                                 <i class="fa fa-shopping-bag fa-2x"></i>
-                                <span class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style="top: -5px; left: 15px; height: 20px; min-width: 20px;">3</span>
+                                {{-- <span class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style="top: -5px; left: 15px; height: 20px; min-width: 20px;">3</span> --}}
                             </a>
 
                             <a href="{{url('profile')}}" class="my-auto">
